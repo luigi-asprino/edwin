da2f7843348a76f44346e65843feae7a57d95185
Luigi Asprino <luigi.asprino@gmail.com>
2019-06-28 15:48:08 +0200

    Ontology

--- src/main/java/it/cnr/istc/stlab/edwin/EquivalenceSetGraph.java
+++ src/main/java/it/cnr/istc/stlab/edwin/EquivalenceSetGraph.java
@@ -1,10 +1,18 @@
 package it.cnr.istc.stlab.edwin;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Set;
 
+import org.apache.jena.vocabulary.RDF;
 import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,8 +28,13 @@ public final class EquivalenceSetGraph {
 	RocksMultiMap<Long, String> IS;
 	RocksMultiMap<Long, Long> H, H_inverse, C, C_inverse;
 	RocksMap<String, Long> oe_size;
-	private String esgFolder;
+	private String esgFolder, equivalencePropertyForProperties, equivalencePropertyToObserve,
+			specializationPropertyToObserve, specializationPropertyForProperties;
 	private static Logger logger = LoggerFactory.getLogger(EquivalenceSetGraph.class);
+	private static final String equivalencePropertyToObserveFile = "equivalencePropertyObserved",
+			specializationPropertyToObserveFile = "specializationPropertyObserved",
+			equivalencePropertyForPropertiesFile = "equivalencePropertyForProperties",
+			specializationPropertyForPropertiesFile = "specializationPropertyForProperties";
 
 	EquivalenceSetGraph(String esgFolder) throws RocksDBException {
 		this.esgFolder = esgFolder;
@@ -36,6 +49,120 @@ public final class EquivalenceSetGraph {
 		C_inverse = new RocksMultiMap<>(esgFolder + "/C_inverse", new LongRocksTransformer(),
 				new LongRocksTransformer());
 		oe_size = new RocksMap<>(esgFolder + "/OE_size", new StringRocksTransformer(), new LongRocksTransformer());
+
+	}
+
+	private void setPropertyFile(String property, String file) {
+		if (!new File(esgFolder + file).exists()) {
+			try {
+				FileOutputStream fos = new FileOutputStream(new File(esgFolder + file));
+				fos.write(property.getBytes());
+				fos.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private String getPropertyFile(String file) throws IOException {
+		if (new File(esgFolder + file).exists()) {
+			BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+			String result = br.readLine();
+			br.close();
+			return result;
+		}
+		return null;
+	}
+
+	public void setEquivalencePropertyToObserve(String equivalencePropertyToObserve) {
+		setPropertyFile(equivalencePropertyToObserve, equivalencePropertyToObserveFile);
+		this.equivalencePropertyToObserve = equivalencePropertyToObserve;
+	}
+
+	public void setSpecializationPropertyToObserve(String specializationPropertyToObserve) {
+		setPropertyFile(specializationPropertyToObserve, specializationPropertyToObserveFile);
+		this.specializationPropertyToObserve = specializationPropertyToObserve;
+	}
+
+	public void setEquivalencePropertyForProperties(String equivalencePropertyForProperties) {
+		setPropertyFile(equivalencePropertyForProperties, equivalencePropertyForPropertiesFile);
+		this.equivalencePropertyForProperties = equivalencePropertyForProperties;
+	}
+
+	public void setSpecializationPropertyForProperties(String specializationPropertyToObserve) {
+		setPropertyFile(specializationPropertyToObserve, specializationPropertyForPropertiesFile);
+		this.specializationPropertyForProperties = specializationPropertyToObserve;
+	}
+
+	public String getEquivalencePropertyToObserve() {
+
+		if (equivalencePropertyToObserve != null) {
+			return equivalencePropertyToObserve;
+		}
+
+		String r = null;
+		try {
+			r = getPropertyFile(equivalencePropertyToObserveFile);
+			this.equivalencePropertyToObserve = r;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return r;
+	}
+
+	public String getSpecializationPropertyToObserve() {
+
+		if (specializationPropertyToObserve != null) {
+			return specializationPropertyToObserve;
+		}
+
+		String r = null;
+		try {
+			r = getPropertyFile(specializationPropertyToObserveFile);
+			specializationPropertyToObserve = r;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return r;
+	}
+
+	public String getEquivalencePropertyForProperties() {
+
+		if (equivalencePropertyForProperties != null) {
+			return equivalencePropertyForProperties;
+		}
+
+		String r = null;
+		try {
+			r = getPropertyFile(equivalencePropertyForPropertiesFile);
+			equivalencePropertyForProperties = r;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return r;
+
+	}
+
+	public String getSpecializationPropertyForProperties() {
+
+		if (specializationPropertyForProperties != null) {
+			return specializationPropertyForProperties;
+		}
+
+		String r = null;
+		try {
+			r = getPropertyFile(specializationPropertyForPropertiesFile);
+			specializationPropertyForProperties = r;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return r;
 
 	}
 
@@ -166,6 +293,72 @@ public final class EquivalenceSetGraph {
 		}
 
 		return visited;
+	}
+
+	public void toRDF(String file, String base, String esgName) throws IOException {
+		FileOutputStream fos = new FileOutputStream(new File(file));
+
+		String esgUri = base + esgName;
+
+		fos.write(
+				getTripleString(esgUri, RDF.type.getURI(), EquivalenceSetGraphOntology.EQUIVALENCESETGRAPH).getBytes());
+		fos.write(getTripleString(esgUri, EquivalenceSetGraphOntology.observesEquivalenceProperty,
+				this.equivalencePropertyToObserve).getBytes());
+		fos.write(getTripleString(esgUri, EquivalenceSetGraphOntology.observesSpecializationProperty,
+				this.specializationPropertyToObserve).getBytes());
+		fos.write(getTripleString(esgUri, EquivalenceSetGraphOntology.equivalencePropertyForPropertiesUsed,
+				this.equivalencePropertyForProperties).getBytes());
+		fos.write(getTripleString(esgUri, EquivalenceSetGraphOntology.specializationPropertyForPropertiesUsed,
+				this.specializationPropertyForProperties).getBytes());
+
+		Iterator<Entry<String, Long>> itID = ID.iterator();
+		while (itID.hasNext()) {
+			Entry<String, Long> entry = itID.next();
+			fos.write(getTripleString(base + entry.getKey(), RDF.type.getURI(), EquivalenceSetGraphOntology.NODE)
+					.getBytes());
+			fos.write(getTripleString(esgUri, EquivalenceSetGraphOntology.HASNODE, base + entry.getValue()).getBytes());
+			fos.write(getTripleString(base + entry.getValue(), EquivalenceSetGraphOntology.CONTAINS, entry.getKey())
+					.getBytes());
+		}
+
+		Iterator<Entry<Long, Collection<String>>> itIS = IS.iterator();
+		while (itIS.hasNext()) {
+			Entry<Long, Collection<String>> entry = itIS.next();
+			for (String s1 : entry.getValue()) {
+				for (String s2 : entry.getValue()) {
+					fos.write(getTripleString(s1, this.equivalencePropertyToObserve, s2).getBytes());
+					fos.write(getTripleString(s2, this.equivalencePropertyToObserve, s1).getBytes());
+				}
+			}
+		}
+
+		Iterator<Entry<Long, Collection<Long>>> itH = H.iterator();
+		while (itH.hasNext()) {
+			Entry<Long, Collection<Long>> entry = itH.next();
+			for (Long l : entry.getValue()) {
+				fos.write(getTripleString(base + entry.getKey(), EquivalenceSetGraphOntology.specializes, base + l)
+						.getBytes());
+				fos.write(getTripleString(base + l, EquivalenceSetGraphOntology.isSpecializedBy, base + entry.getKey())
+						.getBytes());
+			}
+
+		}
+
+		fos.close();
+	}
+
+	private static String getTripleString(String subject, String predicate, String object) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<");
+		sb.append(subject);
+		sb.append("> <");
+		sb.append(predicate);
+		sb.append("> <");
+		sb.append(object);
+		sb.append("> .\n");
+
+		return sb.toString();
 	}
 
 }
---
+++ src/main/java/it/cnr/istc/stlab/edwin/EquivalenceSetGraphOntology.java
@@ -0,0 +1,17 @@
+package it.cnr.istc.stlab.edwin;
+
+public final class EquivalenceSetGraphOntology {
+
+	final static String PREFIX = "https://w3id.org/edwin/ontology/";
+	final static String CONTAINS = PREFIX + "contains";
+	final static String HASNODE = PREFIX + "hasNode";
+	final static String observesEquivalenceProperty = PREFIX + "observesEquivalenceProperty";
+	final static String observesSpecializationProperty = PREFIX + "observesSpecializationProperty";
+	final static String equivalencePropertyForPropertiesUsed = PREFIX + "equivalencePropertyForProperties";
+	final static String specializationPropertyForPropertiesUsed = PREFIX + "specializationPropertyForProperties";
+	final static String NODE = PREFIX + "Node";
+	final static String EQUIVALENCESETGRAPH = PREFIX + "EquivalenceSetGraph";
+	final static String specializes = PREFIX + "specializes";
+	final static String isSpecializedBy = PREFIX + "isSpecializedBy";
+
+}
