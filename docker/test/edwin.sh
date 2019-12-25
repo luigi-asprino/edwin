c27ed76e0a2669a20bf92e325fdc354e0ae601ee
Luigi Asprino <luigi.asprino@gmail.com>
2019-12-25 07:57:40 +0100

    docker mvn package

--- docker/test/edwin.sh
+++ docker/test/edwin.sh
@@ -3,4 +3,5 @@ git pull
 mvn clean install  -Dgpg.skip
 cd /opt/edwin/
 git pull
+mvn package
 mvn exec:java -Dexec.mainClass="it.cnr.istc.stlab.edwin.Edwin" -Dexec.args="/opt/data/conf.properties" -DjvmArgs="-Xmx32g"
