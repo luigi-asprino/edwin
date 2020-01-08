c78365409a2bd25f78ec0924ddf807f91c4129ac
Luigi Asprino <luigi.asprino@gmail.com>
2020-01-08 14:21:35 +0100

    install

--- docker/test/install.sh
+++ docker/test/install.sh
@@ -1,14 +1,19 @@
-cd /opt/
-git clone https://github.com/rdfhdt/hdt-java.git
-cd /opt/hdt-java/ 
+cd /opt
+git clone https://github.com/luigi-asprino/rocks-map.git
+cd /opt/rocks-map
+git checkout 0.0.3.pr
+mvn clean install  -Dgpg.skip
+cd /opt
+git clone https://github.com/luigi-asprino/hdt-java.git
+cd /opt/hdt-java
 mvn clean install
-cd /opt/ 
+cd /opt/
 git clone https://github.com/luigi-asprino/lgu-commons.git
 cd /opt/lgu-commons/
-git checkout dev 
+git checkout dev
 mvn clean install  -Dgpg.skip
 cd /opt/
-git clone https://github.com/luigi-asprino/edwin.git 
+git clone https://github.com/luigi-asprino/edwin.git
 cd /opt/edwin/
 git checkout dev-multiplefiles
 mvn clean install
